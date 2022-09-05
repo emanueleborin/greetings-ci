9365be250cdb4452291d2d7d04a7c185593f0964
Emanuele Borin <33348461+emanueleborin@users.noreply.github.com>
2022-09-05 13:26:29 +0200

    fix: print parameter inside loop

--- test-script.sh
+++ test-script.sh
@@ -6,5 +6,6 @@ java -jar build/libs/greetings-ci-$1.jar ${@:2} > output.bench
 cat output.bench
 IFS=' ' read -ra ARR <<< "${@:2}"
 for i in "${ARR[@]}"; do
+   echo ">$i<"
    grep "^$i$" output.bench
 done
